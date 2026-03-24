89161d469c22bff6f887012aa710da1ea73df289
Mahdi Rahimi <m.s.rahimi@gmail.com>
2026-03-23 17:03:45 -0700

    Add new waitlist and designation visualization

---
+++ data/demand_data.js
@@ -0,0 +1,850 @@
+const DEMAND_DATA = {
+  "tk": [
+    {
+      "school_name": "Clarendon ES",
+      "waitlist": 239,
+      "designated": 0
+    },
+    {
+      "school_name": "Noriega EES",
+      "waitlist": 202,
+      "designated": 0
+    },
+    {
+      "school_name": "West Portal ES",
+      "waitlist": 193,
+      "designated": 0
+    },
+    {
+      "school_name": "Lawton K-8",
+      "waitlist": 173,
+      "designated": 0
+    },
+    {
+      "school_name": "Stevenson (Robert Louis) ES",
+      "waitlist": 136,
+      "designated": 0
+    },
+    {
+      "school_name": "Alvarado ES",
+      "waitlist": 135,
+      "designated": 0
+    },
+    {
+      "school_name": "Chinese Immersion School at DeAvila ES",
+      "waitlist": 132,
+      "designated": 0
+    },
+    {
+      "school_name": "Ulloa ES",
+      "waitlist": 113,
+      "designated": 0
+    },
+    {
+      "school_name": "Flynn (Leonard R) ES",
+      "waitlist": 111,
+      "designated": 0
+    },
+    {
+      "school_name": "Mission Bay ES",
+      "waitlist": 108,
+      "designated": 0
+    },
+    {
+      "school_name": "Huerta (Dolores) ES",
+      "waitlist": 99,
+      "designated": 0
+    },
+    {
+      "school_name": "Grattan ES",
+      "waitlist": 94,
+      "designated": 0
+    },
+    {
+      "school_name": "Rooftop K-8",
+      "waitlist": 82,
+      "designated": 0
+    },
+    {
+      "school_name": "Ortega (Jose) ES",
+      "waitlist": 79,
+      "designated": 0
+    },
+    {
+      "school_name": "Key (Francis Scott) ES",
+      "waitlist": 77,
+      "designated": 0
+    },
+    {
+      "school_name": "King (Thomas Starr) ES",
+      "waitlist": 69,
+      "designated": 0
+    },
+    {
+      "school_name": "Mission Education Center ES",
+      "waitlist": 69,
+      "designated": 0
+    },
+    {
+      "school_name": "Miraloma ES",
+      "waitlist": 62,
+      "designated": 0
+    },
+    {
+      "school_name": "McKinley ES",
+      "waitlist": 56,
+      "designated": 0
+    },
+    {
+      "school_name": "Feinstein (Dianne) ES",
+      "waitlist": 43,
+      "designated": 0
+    },
+    {
+      "school_name": "Argonne ES",
+      "waitlist": 42,
+      "designated": 0
+    },
+    {
+      "school_name": "Alamo ES",
+      "waitlist": 41,
+      "designated": 0
+    },
+    {
+      "school_name": "McCoppin (Frank) ES",
+      "waitlist": 38,
+      "designated": 0
+    },
+    {
+      "school_name": "Milk (Harvey) Civil Right ES",
+      "waitlist": 38,
+      "designated": 0
+    },
+    {
+      "school_name": "Lakeshore ES",
+      "waitlist": 37,
+      "designated": 0
+    },
+    {
+      "school_name": "Argonne EES",
+      "waitlist": 35,
+      "designated": 0
+    },
+    {
+      "school_name": "Sutro ES",
+      "waitlist": 34,
+      "designated": 0
+    },
+    {
+      "school_name": "Lafayette ES",
+      "waitlist": 31,
+      "designated": 0
+    },
+    {
+      "school_name": "San Miguel EES",
+      "waitlist": 31,
+      "designated": 0
+    },
+    {
+      "school_name": "Rodriguez (Zaida T) EES",
+      "waitlist": 19,
+      "designated": 0
+    },
+    {
+      "school_name": "SF Public Montessori ES",
+      "waitlist": 15,
+      "designated": 0
+    },
+    {
+      "school_name": "Sherman ES",
+      "waitlist": 15,
+      "designated": 0
+    },
+    {
+      "school_name": "Taylor (Edward R) ES",
+      "waitlist": 14,
+      "designated": 0
+    },
+    {
+      "school_name": "Lau (Gordon J) ES",
+      "waitlist": 10,
+      "designated": 0
+    },
+    {
+      "school_name": "Stockton (Commodore) EES",
+      "waitlist": 10,
+      "designated": 0
+    },
+    {
+      "school_name": "Moscone (George R) ES",
+      "waitlist": 7,
+      "designated": 0
+    },
+    {
+      "school_name": "Longfellow ES",
+      "waitlist": 5,
+      "designated": 0
+    },
+    {
+      "school_name": "Serra (Junipero) ES",
+      "waitlist": 5,
+      "designated": 0
+    },
+    {
+      "school_name": "Garfield ES",
+      "waitlist": 4,
+      "designated": 0
+    },
+    {
+      "school_name": "Sheridan ES",
+      "waitlist": 4,
+      "designated": 0
+    },
+    {
+      "school_name": "Muir (John) ES",
+      "waitlist": 3,
+      "designated": 0
+    },
+    {
+      "school_name": "SF Community K-8",
+      "waitlist": 6,
+      "designated": 7
+    },
+    {
+      "school_name": "Guadalupe ES",
+      "waitlist": 7,
+      "designated": 8
+    },
+    {
+      "school_name": "Chavez (Cesar) ES",
+      "waitlist": 7,
+      "designated": 9
+    },
+    {
+      "school_name": "Tule Elk Park EES",
+      "waitlist": 90,
+      "designated": 9
+    },
+    {
+      "school_name": "Hillcrest ES",
+      "waitlist": 6,
+      "designated": 10
+    },
+    {
+      "school_name": "Serra (Junipero) Annex EES",
+      "waitlist": 11,
+      "designated": 11
+    },
+    {
+      "school_name": "Carmichael (Bessie) K-8",
+      "waitlist": 3,
+      "designated": 12
+    },
+    {
+      "school_name": "Sanchez ES",
+      "waitlist": 3,
+      "designated": 12
+    },
+    {
+      "school_name": "Visitacion Valley ES",
+      "waitlist": 1,
+      "designated": 14
+    },
+    {
+      "school_name": "Cobb (Dr William L) ES",
+      "waitlist": 1,
+      "designated": 19
+    },
+    {
+      "school_name": "Parks (Rosa) ES",
+      "waitlist": 3,
+      "designated": 19
+    },
+    {
+      "school_name": "Parker (Jean) ES",
+      "waitlist": 1,
+      "designated": 21
+    },
+    {
+      "school_name": "Tenderloin Community ES",
+      "waitlist": 4,
+      "designated": 30
+    },
+    {
+      "school_name": "Harte (Bret) ES",
+      "waitlist": 2,
+      "designated": 33
+    },
+    {
+      "school_name": "Bryant ES",
+      "waitlist": 4,
+      "designated": 34
+    },
+    {
+      "school_name": "Drew (Dr Charles) College Preparatory Academy ES",
+      "waitlist": 3,
+      "designated": 35
+    },
+    {
+      "school_name": "Revere (Paul) K-8",
+      "waitlist": 3,
+      "designated": 36
+    },
+    {
+      "school_name": "Redding ES",
+      "waitlist": 1,
+      "designated": 37
+    },
+    {
+      "school_name": "Spring Valley Science ES",
+      "waitlist": 4,
+      "designated": 38
+    },
+    {
+      "school_name": "Carver (Dr George W) ES",
+      "waitlist": 1,
+      "designated": 40
+    },
+    {
+      "school_name": "Malcolm X Academy ES",
+      "waitlist": 1,
+      "designated": 42
+    },
+    {
+      "school_name": "El Dorado ES",
+      "waitlist": 2,
+      "designated": 44
+    },
+    {
+      "school_name": "McLaren (John) EES",
+      "waitlist": 2,
+      "designated": 71
+    }
+  ],
+  "k": [
+    {
+      "school_name": "Sunset ES",
+      "waitlist": 207,
+      "designated": 0
+    },
+    {
+      "school_name": "Lilienthal (Claire) K-8",
+      "waitlist": 187,
+      "designated": 0
+    },
+    {
+      "school_name": "Clarendon ES",
+      "waitlist": 179,
+      "designated": 0
+    },
+    {
+      "school_name": "Alvarado ES",
+      "waitlist": 165,
+      "designated": 0
+    },
+    {
+      "school_name": "Peabody (George) ES",
+      "waitlist": 152,
+      "designated": 0
+    },
+    {
+      "school_name": "Yu (Alice Fong) K-8",
+      "waitlist": 128,
+      "designated": 0
+    },
+    {
+      "school_name": "West Portal ES",
+      "waitlist": 116,
+      "designated": 0
+    },
+    {
+      "school_name": "Lawton K-8",
+      "waitlist": 109,
+      "designated": 0
+    },
+    {
+      "school_name": "Rooftop K-8",
+      "waitlist": 85,
+      "designated": 0
+    },
+    {
+      "school_name": "New Traditions ES",
+      "waitlist": 76,
+      "designated": 0
+    },
+    {
+      "school_name": "Chinese Immersion School at DeAvila ES",
+      "waitlist": 73,
+      "designated": 0
+    },
+    {
+      "school_name": "Ulloa ES",
+      "waitlist": 73,
+      "designated": 0
+    },
+    {
+      "school_name": "Stevenson (Robert Louis) ES",
+      "waitlist": 66,
+      "designated": 0
+    },
+    {
+      "school_name": "Alamo ES",
+      "waitlist": 58,
+      "designated": 0
+    },
+    {
+      "school_name": "Lafayette ES",
+      "waitlist": 57,
+      "designated": 0
+    },
+    {
+      "school_name": "Webster (Daniel) ES",
+      "waitlist": 54,
+      "designated": 0
+    },
+    {
+      "school_name": "Key (Francis Scott) ES",
+      "waitlist": 53,
+      "designated": 0
+    },
+    {
+      "school_name": "McCoppin (Frank) ES",
+      "waitlist": 50,
+      "designated": 0
+    },
+    {
+      "school_name": "Argonne ES",
+      "waitlist": 47,
+      "designated": 0
+    },
+    {
+      "school_name": "Grattan ES",
+      "waitlist": 47,
+      "designated": 0
+    },
+    {
+      "school_name": "Huerta (Dolores) ES",
+      "waitlist": 40,
+      "designated": 0
+    },
+    {
+      "school_name": "Miraloma ES",
+      "waitlist": 37,
+      "designated": 0
+    },
+    {
+      "school_name": "McKinley ES",
+      "waitlist": 35,
+      "designated": 0
+    },
+    {
+      "school_name": "Milk (Harvey) Civil Right ES",
+      "waitlist": 34,
+      "designated": 0
+    },
+    {
+      "school_name": "Parks (Rosa) ES",
+      "waitlist": 30,
+      "designated": 0
+    },
+    {
+      "school_name": "Mission Bay ES",
+      "waitlist": 29,
+      "designated": 0
+    },
+    {
+      "school_name": "Sherman ES",
+      "waitlist": 23,
+      "designated": 0
+    },
+    {
+      "school_name": "Flynn (Leonard R) ES",
+      "waitlist": 22,
+      "designated": 0
+    },
+    {
+      "school_name": "Feinstein (Dianne) ES",
+      "waitlist": 20,
+      "designated": 0
+    },
+    {
+      "school_name": "SF Public Montessori ES",
+      "waitlist": 20,
+      "designated": 0
+    },
+    {
+      "school_name": "Sutro ES",
+      "waitlist": 20,
+      "designated": 0
+    },
+    {
+      "school_name": "Sloat (Commodore) ES",
+      "waitlist": 18,
+      "designated": 0
+    },
+    {
+      "school_name": "Glen Park ES",
+      "waitlist": 17,
+      "designated": 0
+    },
+    {
+      "school_name": "Sunnyside ES",
+      "waitlist": 16,
+      "designated": 0
+    },
+    {
+      "school_name": "Jefferson ES",
+      "waitlist": 15,
+      "designated": 0
+    },
+    {
+      "school_name": "Visitacion Valley ES",
+      "waitlist": 15,
+      "designated": 0
+    },
+    {
+      "school_name": "Taylor (Edward R) ES",
+      "waitlist": 10,
+      "designated": 0
+    },
+    {
+      "school_name": "Muir (John) ES",
+      "waitlist": 8,
+      "designated": 0
+    },
+    {
+      "school_name": "Sanchez ES",
+      "waitlist": 7,
+      "designated": 0
+    },
+    {
+      "school_name": "Guadalupe ES",
+      "waitlist": 6,
+      "designated": 0
+    },
+    {
+      "school_name": "Wo (Yick) ES",
+      "waitlist": 6,
+      "designated": 0
+    },
+    {
+      "school_name": "Serra (Junipero) ES",
+      "waitlist": 5,
+      "designated": 0
+    },
+    {
+      "school_name": "Cobb (Dr William L) ES",
+      "waitlist": 4,
+      "designated": 0
+    },
+    {
+      "school_name": "Hillcrest ES",
+      "waitlist": 2,
+      "designated": 0
+    },
+    {
+      "school_name": "Carver (Dr George W) ES",
+      "waitlist": 1,
+      "designated": 0
+    },
+    {
+      "school_name": "Buena Vista Horace Mann K-8",
+      "waitlist": 1,
+      "designated": 1
+    },
+    {
+      "school_name": "Marshall ES",
+      "waitlist": 0,
+      "designated": 1
+    },
+    {
+      "school_name": "Monroe ES",
+      "waitlist": 21,
+      "designated": 1
+    },
+    {
+      "school_name": "Longfellow ES",
+      "waitlist": 5,
+      "designated": 2
+    },
+    {
+      "school_name": "Moscone (George R) ES",
+      "waitlist": 8,
+      "designated": 2
+    },
+    {
+      "school_name": "Ortega (Jose) ES",
+      "waitlist": 83,
+      "designated": 2
+    },
+    {
+      "school_name": "Spring Valley Science ES",
+      "waitlist": 5,
+      "designated": 2
+    },
+    {
+      "school_name": "Chavez (Cesar) ES",
+      "waitlist": 9,
+      "designated": 3
+    },
+    {
+      "school_name": "Drew (Dr Charles) College Preparatory Academy ES",
+      "waitlist": 4,
+      "designated": 3
+    },
+    {
+      "school_name": "Harte (Bret) ES",
+      "waitlist": 4,
+      "designated": 4
+    },
+    {
+      "school_name": "SF Community K-8",
+      "waitlist": 2,
+      "designated": 4
+    },
+    {
+      "school_name": "Garfield ES",
+      "waitlist": 2,
+      "designated": 6
+    },
+    {
+      "school_name": "King (Thomas Starr) ES",
+      "waitlist": 61,
+      "designated": 6
+    },
+    {
+      "school_name": "Lau (Gordon J) ES",
+      "waitlist": 1,
+      "designated": 6
+    },
+    {
+      "school_name": "Tenderloin Community ES",
+      "waitlist": 5,
+      "designated": 6
+    },
+    {
+      "school_name": "Bryant ES",
+      "waitlist": 9,
+      "designated": 7
+    },
+    {
+      "school_name": "Chin (John Yehall) ES",
+      "waitlist": 0,
+      "designated": 7
+    },
+    {
+      "school_name": "El Dorado ES",
+      "waitlist": 2,
+      "designated": 7
+    },
+    {
+      "school_name": "Parker (Jean) ES",
+      "waitlist": 0,
+      "designated": 8
+    },
+    {
+      "school_name": "Revere (Paul) K-8",
+      "waitlist": 4,
+      "designated": 8
+    },
+    {
+      "school_name": "Carmichael (Bessie) K-8",
+      "waitlist": 3,
+      "designated": 14
+    },
+    {
+      "school_name": "Cleveland ES",
+      "waitlist": 6,
+      "designated": 17
+    },
+    {
+      "school_name": "Redding ES",
+      "waitlist": 6,
+      "designated": 17
+    },
+    {
+      "school_name": "Lakeshore ES",
+      "waitlist": 9,
+      "designated": 20
+    },
+    {
+      "school_name": "Sheridan ES",
+      "waitlist": 4,
+      "designated": 23
+    }
+  ],
+  "g6": [
+    {
+      "school_name": "Giannini (A.P.) MS",
+      "waitlist": 414,
+      "designated": 0
+    },
+    {
+      "school_name": "Roosevelt MS",
+      "waitlist": 304,
+      "designated": 0
+    },
+    {
+      "school_name": "Presidio MS",
+      "waitlist": 215,
+      "designated": 0
+    },
+    {
+      "school_name": "Aptos MS",
+      "waitlist": 211,
+      "designated": 0
+    },
+    {
+      "school_name": "Hoover (Herbert) MS",
+      "waitlist": 186,
+      "designated": 0
+    },
+    {
+      "school_name": "Lawton K-8",
+      "waitlist": 117,
+      "designated": 0
+    },
+    {
+      "school_name": "Lilienthal (Claire) K-8",
+      "waitlist": 80,
+      "designated": 0
+    },
+    {
+      "school_name": "Rooftop K-8",
+      "waitlist": 65,
+      "designated": 0
+    },
+    {
+      "school_name": "Brown Jr. (Willie) MS",
+      "waitlist": 63,
+      "designated": 0
+    },
+    {
+      "school_name": "Yu (Alice Fong) K-8",
+      "waitlist": 28,
+      "designated": 0
+    },
+    {
+      "school_name": "Buena Vista Horace Mann K-8",
+      "waitlist": 22,
+      "designated": 0
+    },
+    {
+      "school_name": "SF Community K-8",
+      "waitlist": 9,
+      "designated": 0
+    },
+    {
+      "school_name": "Carmichael (Bessie) K-8",
+      "waitlist": 5,
+      "designated": 6
+    },
+    {
+      "school_name": "Visitacion Valley MS",
+      "waitlist": 1,
+      "designated": 10
+    },
+    {
+      "school_name": "King Jr (Dr Martin L) MS",
+      "waitlist": 0,
+      "designated": 11
+    },
+    {
+      "school_name": "Revere (Paul) K-8",
+      "waitlist": 1,
+      "designated": 11
+    },
+    {
+      "school_name": "Marina MS",
+      "waitlist": 9,
+      "designated": 23
+    },
+    {
+      "school_name": "Francisco MS",
+      "waitlist": 3,
+      "designated": 24
+    },
+    {
+      "school_name": "Everett MS",
+      "waitlist": 5,
+      "designated": 27
+    },
+    {
+      "school_name": "Lick (James) MS",
+      "waitlist": 4,
+      "designated": 27
+    },
+    {
+      "school_name": "Denman (James) MS",
+      "waitlist": 2,
+      "designated": 57
+    }
+  ],
+  "g9": [
+    {
+      "school_name": "Lincoln (Abraham) HS",
+      "waitlist": 784,
+      "designated": 0
+    },
+    {
+      "school_name": "Washington (George) HS",
+      "waitlist": 615,
+      "designated": 0
+    },
+    {
+      "school_name": "Asawa (Ruth) SOTA HS",
+      "waitlist": 208,
+      "designated": 0
+    },
+    {
+      "school_name": "Balboa HS",
+      "waitlist": 186,
+      "designated": 0
+    },
+    {
+      "school_name": "Lowell HS",
+      "waitlist": 18,
+      "designated": 0
+    },
+    {
+      "school_name": "Marshall (Thurgood) HS",
+      "waitlist": 8,
+      "designated": 0
+    },
+    {
+      "school_name": "Jordan (June) HS",
+      "waitlist": 6,
+      "designated": 0
+    },
+    {
+      "school_name": "O'Connell (John) HS",
+      "waitlist": 6,
+      "designated": 0
+    },
+    {
+      "school_name": "SF International HS",
+      "waitlist": 2,
+      "designated": 0
+    },
+    {
+      "school_name": "Mission HS",
+      "waitlist": 21,
+      "designated": 24
+    },
+    {
+      "school_name": "Galileo HS",
+      "waitlist": 29,
+      "designated": 36
+    },
+    {
+      "school_name": "Wallenberg (Raoul) HS",
+      "waitlist": 12,
+      "designated": 39
+    },
+    {
+      "school_name": "Burton (Phillip and Sala) HS",
+      "waitlist": 17,
+      "designated": 77
+    }
+  ]
+};
